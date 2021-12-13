9c8e3411676c7191b20dafe418038e1cb40a27af
Team Monster <90180469+MayilGaming@users.noreply.github.com>
2021-12-13 17:16:03 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,12 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [MG Bots Updates](
+https://t.me/mgbotslists)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ My Creator : [Click Me](https://t.me/MayilM07)
 """
